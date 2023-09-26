5a536cbe4a364e4a6339304b9f9eb1ac42d9652e
AranPrado <aranpradoamaral@gmail.com>
2023-09-26 15:02:58 -0300

    Criacao da startup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using FitFusion;
 using FitFusion.Controllers;
 using FitFusion.Database;
 using FitFusion.Repositores;
@@ -6,40 +7,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//Conexão com o banco de dados
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("DataBase"),
-    new MySqlServerVersion(new Version(8, 0, 26)),
-    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
-
-//fim
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IExerciciosRepositore, ExerciciosController>();
-builder.Services.AddScoped<ITreinosRepositore, TreinoController>();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
-    });
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+using FitFusion.Controllers;
+using FitFusion.Database;
+using FitFusion.Repositores;
+using FitFusion.Repositores.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitFusion
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var connectionString = Configuration.GetConnectionString("DataBase");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(connectionString,
+                    new MySqlServerVersion(new Version(8, 0, 26)),
+                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddScoped<IExerciciosRepositore, ExerciciosController>();
+            services.AddScoped<ITreinosRepositore, TreinoController>();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
