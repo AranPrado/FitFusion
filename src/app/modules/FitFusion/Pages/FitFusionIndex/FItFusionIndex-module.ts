2b27c8dc8fb6d30be6a18f5fab501e02fdfe399e
AranPrado <aranpradoamaral@gmail.com>
2023-11-13 23:09:16 -0300

    Finalizado Index

--- src/app/modules/FitFusion/Pages/FitFusionIndex/FItFusionIndex-module.ts
+++ src/app/modules/FitFusion/Pages/FitFusionIndex/FItFusionIndex-module.ts
@@ -3,11 +3,12 @@ import { CardComponent } from "../../components/Card/Card.component";
 import { FitFusionIndexComponent } from "./FitFusionIndex.component";
 import { RouterModule } from "@angular/router";
 import { CardPerfilComponent } from "../../components/CardPerfil/CardPerfil.component";
+import { CardNoticiasComponent } from "../../components/CardNoticias/CardNoticias.component";
 
 
 
 @NgModule({
-    declarations: [CardComponent,FitFusionIndexComponent,CardPerfilComponent],
+    declarations: [CardComponent,FitFusionIndexComponent,CardPerfilComponent, CardNoticiasComponent],
     imports: [RouterModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
---
+++ src/app/modules/FitFusion/components/CardNoticias/CardNoticias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-CardNoticias',
+  templateUrl: './CardNoticias.component.html',
+  styleUrls: ['./CardNoticias.component.css']
+})
+export class CardNoticiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
