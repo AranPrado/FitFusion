d45679caa5de4dc13c7ab191a69529196ed8b6dc
AranPrado <aranpradoamaral@gmail.com>
2023-11-21 15:00:34 -0300

    NavBar transformada em componente

--- src/app/modules/FitFusion/Pages/FitFusionIndex/FItFusionIndex-module.ts
+++ src/app/modules/FitFusion/Pages/FitFusionIndex/FItFusionIndex-module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from "@angular/router";
 import { CardPerfilComponent } from "../../components/CardPerfil/CardPerfil.component";
 import { CardNoticiasComponent } from "../../components/CardNoticias/CardNoticias.component";
 import { FitFusionServicesService } from "../../services/FitFusionServices.service";
+import { NavBarComponent } from "../../components/NavBar/NavBar.component";
 
 
 
 @NgModule({
-    declarations: [CardComponent,FitFusionIndexComponent,CardPerfilComponent, CardNoticiasComponent],
+    declarations: [CardComponent,FitFusionIndexComponent,CardPerfilComponent, CardNoticiasComponent,NavBarComponent],
     imports: [RouterModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     providers:[FitFusionServicesService]
